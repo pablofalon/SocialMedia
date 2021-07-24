41c738cdafde1e146857d624ed51ab25ed3fcf32
Pablo <Pablo@PABLOPC>
2021-07-24 11:40:28 -0300

    Add project files.

---
+++ SocialMedia.API/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostRepository _repository;
+        public PostController(IPostRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPosts()
+        {
+            var posts = await _repository.GetPosts(); ;
+            return Ok(posts);
+        }
+    }
+}
