241ed393f591a8b3ecbbbaa36a672050fbb7fe1c
Pablo Falón <pablo.falon@outlook.com>
2021-07-24 20:02:02 -0300

    Add automapper and DTOS

---
+++ SocialMedia.Infrastructure/Mappings/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SocialMedia.Core.DTOs;
+using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.API.Mappings
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<PostDTO, Post>();
+        } 
+    }
+}
